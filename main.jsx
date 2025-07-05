2e3e88c808eb7f08da724d9413e57a88c1e949ac
TrueValPoperty <williamtylerstreet@gmail.com>
2025-07-05 20:04:37 +0100

    Add files via upload

--- main.jsx
+++ main.jsx
@@ -5,17 +5,15 @@ import 'leaflet/dist/leaflet.css'
 
 function App() {
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: '100vh', width: '100%' }}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution="&copy; OpenStreetMap contributors"
-      />
-    </MapContainer>
+    <div style={{ height: "100vh", width: "100%" }}>
+      <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: "100%", width: "100%" }}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution="&copy; OpenStreetMap contributors"
+        />
+      </MapContainer>
+    </div>
   )
 }
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
