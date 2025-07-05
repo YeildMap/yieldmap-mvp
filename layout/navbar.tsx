207644a663335e58307ec30f4fdd36ef1516050b
TrueValPoperty <williamtylerstreet@gmail.com>
2025-07-05 12:35:19 +0100

    Create navbar.tsx

---
+++ layout/navbar.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+/**
+ * Simple Navbar component.
+ *
+ * Extend this component to add navigation links or branding
+ * according to your application's needs.
+ */
+export default function Navbar() {
+  return (
+    <nav style={{ padding: '1rem', backgroundColor: '#f2f2f2' }}>
+      <h1>Navbar</h1>
+    </nav>
+  );
+}
